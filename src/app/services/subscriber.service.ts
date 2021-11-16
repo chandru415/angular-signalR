f05f6a194c0ef31c5bebbb0212c6a9b274a76beb
chandru415 <chandru9052@gmail.com>
2021-11-16 16:25:10 +0530

    [chandu] Implemented SignalR connection

--- src/app/services/subscriber.service.ts
+++ src/app/services/subscriber.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import * as SignalR from '@microsoft/signalr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SubscriberService {
+  private readonly hubURL = `${environment.signalR_URL}/chart`;
+  private hubConnection!: SignalR.HubConnection;
 
-  private readonly hubURL = `${environment.signalR_URL}/chart`
+  constructor(private http: HttpClient) {this.startSignalRConnection();}
 
-  constructor(private http: HttpClient) { }
+  startSignalRConnection = () => {
+    this.hubConnection = new SignalR.HubConnectionBuilder()
+      .withUrl(this.hubURL)
+      .build();
+
+    this.hubConnection
+      .start()
+      .then(() => console.log(`Connetion Start - ${this.hubURL}`))
+      .catch((err) => console.log(`Error while starting connection - ${err}`));
+  };
 }
