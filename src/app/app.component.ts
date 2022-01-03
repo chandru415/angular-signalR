f674aa5ff26b01edbe2ec31fd96743992f381891
chandru415 <chandru9052@gmail.com>
2022-01-03 19:54:32 +0530

    [chandu] configured signalr with on demand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SubscriberService } from './services/subscriber.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-signalr';
+  connectionState: any;
+  routePath = '';
+  messageData = null;
+  signalRMessageDataCaller = null;
+  stronglyMethodToSubscribe = '';
+  methodToSubscribe = 'transferchartdata';
+  methodToCall = '';
+  paramsBody = '';
+  signalRMessageDataContinuos: any;
 
   constructor(private subService: SubscriberService) {}
+
+  ngOnInit(): void {
+    this.subService.signalRMessageData.subscribe(
+      (data) => (this.messageData = data)
+    );
+    this.subService.connectionState.subscribe(
+      (data) => (this.connectionState = data)
+    );
+    this.subService.signalRMessageDataCaller.subscribe(
+      (data) => (this.signalRMessageDataCaller = data)
+    );
+    this.subService.signalRMessageDataContinuos.subscribe(
+      (data) => (this.signalRMessageDataContinuos = data)
+    );
+  }
+
+  connect = () => {
+    this.subService.startRoutedSignalRConnection(this.routePath);
+  };
+
+  receive = () => {
+    this.subService.subscriptionOnMethod(this.stronglyMethodToSubscribe);
+  };
+
+  callSever = () => {
+    this.subService.sendDataToServer(this.methodToCall,JSON.parse(this.paramsBody.toString()));
+  };
+
+  continuos = () => {
+    this.subService.continuos(this.methodToSubscribe).subscribe();
+  };
+
+  getAlert = () => {
+    switch (this.connectionState.toLowerCase()) {
+      case 'not connected':
+        return { 'alert-warning': true };
+
+      case 'connected':
+        return { 'alert-success': true };
+      case 'disconnected':
+        return { 'alert-danger': true };
+
+      default:
+        return { 'alert-info': true };
+    }
+  };
 }
--- src/app/services/subscriber.service.ts
+++ src/app/services/subscriber.service.ts
@@ -2,35 +2,77 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import * as SignalR from '@microsoft/signalr';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SubscriberService {
-  private readonly hubURL = `${environment.signalR_URL}/chart`;
+  private readonly hubURL = `${environment.signalR_URL}`;
   private hubConnection!: SignalR.HubConnection;
+  connectionState = new BehaviorSubject<string>(
+    this.hubConnection ? this.hubConnection.state : 'Not connected'
+  );
+  signalRMessageData = new BehaviorSubject<any>(null);
+  signalRMessageDataCaller = new BehaviorSubject<any>(null);
+  signalRMessageDataContinuos = new BehaviorSubject<any>(null);
 
   /** For load balancer settings */
   private srHubOptions: SignalR.IHttpConnectionOptions = {
     skipNegotiation: true,
-    transport: 1
-  }
+    transport: 1,
+  };
 
-  constructor(private http: HttpClient) {this.startSignalRConnection();}
+  constructor(private http: HttpClient) {}
 
-  startSignalRConnection = () => {
+  startRoutedSignalRConnection = (route: string) => {
     this.hubConnection = new SignalR.HubConnectionBuilder()
-      .withUrl(this.hubURL, this.srHubOptions)
+      .withUrl(`${this.hubURL}/${route}`, this.srHubOptions)
       .withAutomaticReconnect({
-        nextRetryDelayInMilliseconds: retryContext => {
-          return retryContext.previousRetryCount <= 10 ? Math.random() * 10000 : null
-        }
+        nextRetryDelayInMilliseconds: (retryContext) => {
+          return retryContext.previousRetryCount <= 10
+            ? Math.random() * 10000
+            : null;
+        },
       })
       .build();
 
     this.hubConnection
       .start()
-      .then(() => console.log(`Connetion Start - ${this.hubURL}`))
-      .catch((err) => console.log(`Error while starting connection - ${err}`));
+      .then(() => {
+        console.log(`Connection Start - ${this.hubURL}`);
+        this.connectionState.next(this.hubConnection.state);
+      })
+      .catch((err) => {
+        console.log(`Error while starting connection - ${err}`);
+        this.connectionState.next(this.hubConnection.state);
+      });
+
+    this.hubConnection.onclose(() =>
+      this.connectionState.next(this.hubConnection.state)
+    );
+    this.hubConnection.onreconnected(() =>
+      this.connectionState.next(this.hubConnection.state)
+    );
+  };
+
+  subscriptionOnMethod = (method: string) => {
+    this.hubConnection.invoke(method).then((data) => {
+      this.signalRMessageData.next(data);
+    });
+  };
+
+  sendDataToServer = (method: string, args?: any) => {
+    this.hubConnection.invoke(method, args).then((res) => {
+      this.signalRMessageDataCaller.next({...res});
+    });
+  };
+
+  continuos = (method: string) => {
+    this.hubConnection.on(method, (data) => {
+      this.signalRMessageDataContinuos.next(data);
+    });
+
+    return this.http.get(`https://localhost:44382/api/chart/SendChartData`);
   };
 }
